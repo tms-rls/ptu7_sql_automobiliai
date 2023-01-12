bbf91e35d73a54c9e3f382cae1a7132aafa3fca9
tms <tomas.rls@gmail.com>
2023-01-12 17:16:08 +0200

    Patobulintas rezultato atvaizdavimas

--- uzduotis_2_2.py
+++ uzduotis_2_2.py
@@ -8,22 +8,24 @@ import sqlite3
 konektorius = sqlite3.connect("uzduotis_20230112_1.db")
 kursorius = konektorius.cursor()
 
-marke_paieska = str(input("Įveskite automobilio markę: "))
-modelis_paieska = str(input("Įveskite automobilio modelį: "))
-spalva_paieska = str(input("Įveskite automobilio spalvą: "))
-metai_nuo = str(input("Įveskite automobilio pagaminimo metus nuo: "))
-metai_iki = str(input("Įveskite automobilio pagaminimo metus iki: "))
-kaina_nuo = float(input("Įveskite automobilio kainą nuo: "))
-kaina_iki = float(input("Įveskite automobilio kainą iki: "))
+marke_paieska = input("Įveskite automobilio markę: ")
+modelis_paieska = input("Įveskite automobilio modelį: ")
+spalva_paieska = input("Įveskite automobilio spalvą: ")
+metai_nuo = input("Įveskite automobilio pagaminimo metus nuo: ")
+metai_iki = input("Įveskite automobilio pagaminimo metus iki: ")
+kaina_nuo = input("Įveskite automobilio kainą nuo: ")
+kaina_iki = input("Įveskite automobilio kainą iki: ")
 
 paieska = (marke_paieska, modelis_paieska, spalva_paieska, metai_nuo, metai_iki, kaina_nuo, kaina_iki)
 
-with konektorius:
-    kursorius.execute("SELECT * FROM automobiliai WHERE marke = ? AND modelis = ? AND spalva= ?"
-                      "AND pagaminimo_metai BETWEEN ? AND ? AND kaina BETWEEN ? AND ?", paieska)
-    rezultatas = kursorius.fetchall()
 
-if rezultatas:
-    print(rezultatas)
+with konektorius:
+    kursorius.execute("SELECT * FROM automobiliai WHERE marke = ? OR modelis = ? OR spalva= ?"
+                      "OR pagaminimo_metai BETWEEN ? AND ? OR kaina BETWEEN ? AND ?", paieska)
+    rezultatai = kursorius.fetchall()
+
+if rezultatai:
+    for rezultatas in rezultatai:
+        print(rezultatas)
 else:
     print("Nėra automobilių pagal Jūsų kriterijus!")
